c4a7dd4024307d96f5d531755faeebbbb843a910
tawfik000010 <27507798+tawfik000010@users.noreply.github.com>
2025-12-31 22:59:27 +0530

    05 / joinStrings

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,6 +9,17 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+let fullName = firstName + ' ' + lastName;
+let age = thisYear - birthYear;
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
+console.log (greeting);
+
+
 
 
 
