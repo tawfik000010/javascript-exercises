1214d27b41d02e488d57ef1febf998f4e335a7d5
tawfik000010 <27507798+tawfik000010@users.noreply.github.com>
2026-01-24 23:50:08 +0530

    complete task 07

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let str = ""
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i=0; i < num; i++) {
+        str += string;
+    }
+    return str;
 
 };
+
+let one = "powerRangers";
+console.log(repeatString(one, 17));
 
 // Do not edit below this line
 module.exports = repeatString;
