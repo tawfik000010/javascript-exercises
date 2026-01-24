61d3d96afb1fba5d317c6520317f24211f430239
tawfik000010 <27507798+tawfik000010@users.noreply.github.com>
2026-01-25 00:15:28 +0530

    complete task #7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let whatever = ""
+    for (let i = str.length -1; i >= 0; i--) {
+        whatever += str[i];
+    }
+    return whatever;
 
 };
+
+let wow = "not your type";
+console.log(reverseString(wow));
 
 // Do not edit below this line
 module.exports = reverseString;
