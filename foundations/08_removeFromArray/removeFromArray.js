868e06569cd66ff1bee6d0a7499cc13154e04d32
tawfik000010 <27507798+tawfik000010@users.noreply.github.com>
2026-01-25 17:31:12 +0530

    complete #8 task

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rm) {
+    return arr.filter(item => !rm.includes(item));
 };
-
+let ahhh = [1,2,3,4,5,6,7,8];
+console.log(removeFromArray(ahhh, 972));
 // Do not edit below this line
 module.exports = removeFromArray;
