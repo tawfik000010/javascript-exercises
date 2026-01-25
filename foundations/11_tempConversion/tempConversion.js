9109d9f9082ee6eded95f7bbe6a3179482e44a6a
tawfik000010 <27507798+tawfik000010@users.noreply.github.com>
2026-01-26 02:43:00 +0530

    complete task #11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f-32)*5/9;
+  const round = Math.round(c*10)/10;
+  return round;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = (c*9/5)+32;
+  const round = Math.round(f*10)/10;
+  return round;
 };
 
 // Do not edit below this line
