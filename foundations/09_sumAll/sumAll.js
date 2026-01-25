11dadf7d190e172c226c67dc8ff9b053a143dc9a
tawfik000010 <27507798+tawfik000010@users.noreply.github.com>
2026-01-25 19:40:08 +0530

    complete task #9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let temp =0;
+    if (a > b) {
+        temp = a;
+        a = b;
+        b = temp;  
+    }
+    while (a <= b) {
+        sum += a;
+        a++;
+    }  
+    return sum;
 };
 
 // Do not edit below this line
